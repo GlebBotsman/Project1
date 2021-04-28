71f90345fb9ece0c0ef86a6787f2816535077631
GlebBotsman <alekseev.gleb1@mail.ru>
2021-04-28 16:05:07 +0300

    new project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            var userName = Console.ReadLine();
+            Console.WriteLine("You name is" + userName);
+
+            Console.WriteLine("[" + DateTime.Now + "] " + "Hello, " + userName + "!");
+        }
+        
+         
+        }
+    }
+
